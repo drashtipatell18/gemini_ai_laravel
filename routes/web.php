fa6891622d970098142267df1e46cefe3756dacb
Dhruvish <165013408+drashtipatell18@users.noreply.github.com>
2025-08-21 14:05:21 +0530

    route change index to /

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/login', [LoginController::class, 'Login'])->name('login');
 Route::post('/loginStore', [LoginController::class, 'LoginStore'])->name('loginstore');
@@ -14,5 +11,5 @@ Route::get('/signup', [LoginController::class, 'Signup'])->name('signup');
 
 Route::post('/signupStore', [LoginController::class, 'SignupStore'])->name('signupStore');
 
-Route::get('index', [HomeController::class, 'index'])->name('index');
+Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('upgrade', [HomeController::class, 'Upgrade'])->name('upgrade');
